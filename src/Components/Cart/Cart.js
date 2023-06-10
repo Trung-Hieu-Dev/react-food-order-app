cf73db2a7dea5efb93bf1e45882b32d8d9d17641
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-10 23:22:16 +0700

    Removing Cart Item

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -11,8 +11,12 @@ export const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = () => {};
-  const cartItemAddHandler = () => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
